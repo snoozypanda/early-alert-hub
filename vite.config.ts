79153c4af7fe5eeb447fe01cf64bb56b31a8ba92
coleYab <yabume123@gmail.com>
2026-01-18 12:51:59 +0300

    fix: fixed vite.config.ts to add the allowed hosts and removed the duplicated default exports

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,24 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // ...
-  server: {
-    allowedHosts: ["early-alert-hub-1.onrender.com"],
-  },
-});
+// export default defineConfig({
+//   // ...
+//   server: {
+//     allowedHosts: ["early-alert-hub-1.onrender.com"],
+//   },
+// });
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: [
+      "early-alert-hub-1.onrender.com",
+      "early-alert-hub.vercel.app",
+    ],
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
